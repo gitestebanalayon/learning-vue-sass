deb34faacd518966a78238dac130aa1bd1343ed4
esteban <gitesteban0@gmail.com>
2024-02-20 10:24:44 -0400

    learning vue and sass a1

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>Ok</h1>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non alias illo totam ipsam, unde repudiandae suscipit. Distinctio cum numquam dicta veniam et saepe dolorum sequi vitae soluta, odit beatae iusto.</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    
+    h1{
+        color: red;
+    }
+
+    div{
+        p{
+            color: red;
+        }
+    }
+</style>
