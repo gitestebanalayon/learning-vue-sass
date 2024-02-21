d277bcc6944a50f0ef59ad030c28ae6e095c38fb
esteban <gitesteban0@gmail.com>
2024-02-20 22:29:07 -0400

    learning vue and sass a2

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <script setup>
-
+    import TopNav from './components/TopNav.vue'
 </script>
 
 <template>
-    <h1>Ok</h1>
-    <div>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non alias illo totam ipsam, unde repudiandae suscipit. Distinctio cum numquam dicta veniam et saepe dolorum sequi vitae soluta, odit beatae iusto.</p>
+    <div class="nav-top-global">
+        <TopNav/>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    
-    h1{
-        color: red;
-    }
-
-    div{
-        p{
-            color: red;
-        }
-    }
+    @import './assets/sass/index.scss';
 </style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import NavLeft from './top-nav/top-nav-left/NavLeft.vue'
+    import NavRight from './top-nav/top-nav-right/NavRight.vue'
+</script>
+
+<template>
+    <div class="nav-top">
+        <!--Nav Left-->
+        <NavLeft/>
+        <!--Nav Right-->
+        <NavRight/>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    @import '../assets/sass/partials/variables';
+    @import '../assets/sass/partials/mixings';
+    .nav-top{
+        @include flex-justify-between;
+        width: 100%;
+        height: $em-primary;
+    }
+</style>
